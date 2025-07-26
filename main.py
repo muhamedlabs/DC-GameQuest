a3911e92c07acc8fa2dc66ad4453b938cc448880
Muhamed Ads <kg7221996@gmail.com>
2025-07-26 21:58:09 +0300

    Renovation how

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ import datetime
 import ashredis 
 from dotenv import load_dotenv
 from disnake.ext import commands
-from BANNED_FILES.config import discord_bot
+from BANNED_FILES.config import discord_bot, DISCORD_ID
 
 # Загрузка переменных окружения
 load_dotenv()
@@ -19,7 +19,7 @@ intents.voice_states = True
 intents.guilds = True
 
 # Инициализация бота
-bot = commands.Bot(command_prefix="!", intents=intents)
+bot = commands.Bot(command_prefix="!", intents=intents, test_guilds=[DISCORD_ID])
 
 bot.start_time = datetime.datetime.utcnow()
 
@@ -31,29 +31,37 @@ async def on_ready():
 # Загружаем коги
 bot.load_extension("commands.status_cog") # Папка статус
 
-bot.load_extension("commands.speaker_cog") # Папка спикер
+# bot.load_extension("commands.speaker_cog") # Папка спикер
 
-bot.load_extension("commands.design_cog") # Папка с дизайном профилей дискорда
+# bot.load_extension("commands.design_cog") # Папка с дизайном профилей дискорда
 
-bot.load_extension("commands.telegram_cog") # Папка с подключения постинга из Telegram
+# bot.load_extension("commands.telegram_cog") # Папка с подключения постинга из Telegram
+
+bot.load_extension("commands.advertisement_cog") # Папка с саморекламой от бота
 
 bot.load_extension("commands.messages_cog") # Папка с сообщениями от бота
 
+bot.load_extension("commands.dm_cog") # Папка с личными сообщениями от бота
+
 bot.load_extension("commands.ember_cog") # Папка с ембитам для отправки
 
-bot.load_extension("commands.events_cog") # Папка с событиями на сервере и боте
+# bot.load_extension("commands.events_cog") # Папка с событиями на сервере и боте
 
-bot.load_extension("commands.reaction_cog") # Папка с рекциями на сообщения
+# bot.load_extension("commands.reaction_cog") # Папка с рекциями на сообщения
 
-bot.load_extension("commands.moderation_cog") # Папка с модерацией на сервере и в боте
+# bot.load_extension("commands.moderation_cog") # Папка с модерацией на сервере и в боте
 
-bot.load_extension("commands.information_cog") # Папка с информация о сервере
+# bot.load_extension("commands.information_cog") # Папка с информация о сервере
 
-bot.load_extension("commands.secrecy_cog") # Папка с секретными командами через !
+# bot.load_extension("commands.secrecy_cog") # Папка с секретными командами через !
 
-bot.load_extension("commands.primary_cog") # Папка с особо-основными действиями бота
+# bot.load_extension("commands.primary_cog") # Папка с особо-основными действиями бота
 
-bot.load_extension("commands.database_cog.models") # Папка с выгрузкой из Redis
+# bot.load_extension("commands.database_cog.models") # Папка с выгрузкой из Redis
+
+bot.load_extension("commands.button_cog") # Папка с кнопками по айдишке
+
+bot.load_extension("commands.private_cog")
 
 
 # Запуск
