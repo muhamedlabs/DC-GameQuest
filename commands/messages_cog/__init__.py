8b53de7e6559f271ffeec2c002d58f1c12a282f7
Muhamed Ads <kg7221996@gmail.com>
2025-07-26 22:19:41 +0300

    updating message downloads on the server

--- commands/messages_cog/__init__.py
+++ commands/messages_cog/__init__.py
@@ -1,14 +1,9 @@
 from disnake.ext import commands
-from .responder import DMResponder
 from .greeting import GreetingResponder
 from .handler import WelcomeHandler
-from .promo import AutoPromo
-from .notification import FirstNotifier
+
 
 
 def setup(bot: commands.Bot):
-    bot.add_cog(DMResponder(bot))
     bot.add_cog(GreetingResponder(bot))
     bot.add_cog(WelcomeHandler(bot))
-    bot.add_cog(AutoPromo(bot))
-    bot.add_cog(FirstNotifier(bot))
