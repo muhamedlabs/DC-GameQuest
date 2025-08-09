e8dbc3b1f26f8afcc7255975b8a875f37875cb18
Muhamed Ads <kg7221996@gmail.com>
2025-08-09 04:02:03 +0300

    what a fix

--- commands/status_cog/__init__.py
+++ commands/status_cog/__init__.py
@@ -1,11 +1,9 @@
 import disnake
 from disnake.ext import commands
 from .activity import StatusBot
-from .text import TextCommands
 from .restart import ReloadAllCog
 
 
 def setup(bot: commands.Bot):
     bot.add_cog(StatusBot(bot))
-    bot.add_cog(TextCommands(bot))
     bot.add_cog(ReloadAllCog(bot))
