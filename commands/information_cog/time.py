3ab12c9d17278287486e80198226659d294cb725
muhamedlabs <kg7221996@gmail.com>
2026-06-21 11:58:47 +0300

    Configuring the Time Operator

---
+++ commands/information_cog/time.py
@@ -0,0 +1,9 @@
+import disnake
+from datetime import datetime, timedelta
+from BANNED_FILES.config import Time_interval
+
+
+moscow_time = (datetime.utcnow() + timedelta(hours=3)).strftime("%Y-%m-%d %H:%M:%S")
+kyiv_time = (datetime.utcnow() + timedelta(hours=2)).strftime("%Y-%m-%d %H:%M:%S")
+
+time = (datetime.utcnow() + timedelta(hours=Time_interval)).strftime("%Y-%m-%d %H:%M:%S")
