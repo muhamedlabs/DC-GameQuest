d4daa41190700aa6b4dd315d23101203e32754b1
muhamedlabs <kg7221996@gmail.com>
2026-07-23 21:31:17 +0300

    new date format

--- commands/information_cog/time.py
+++ commands/information_cog/time.py
@@ -2,11 +2,13 @@ import disnake
 from datetime import datetime, timedelta
 from BANNED_FILES.config import Time_interval
 
-
-
 moscow_time = (datetime.utcnow() + timedelta(hours=3)).strftime("%Y-%m-%d %H:%M:%S")
+
 kyiv_time = (datetime.utcnow() + timedelta(hours=2)).strftime("%Y-%m-%d %H:%M:%S")
 
-
-
 hours_time = (datetime.utcnow() + timedelta(hours=Time_interval)).strftime("%d.%m.%Y %H:%M:%S")
+
+
+
+def current_time() -> str:
+    return (datetime.utcnow() + timedelta(hours=Time_interval)).strftime("%d.%m.%Y %H:%M:%S")
