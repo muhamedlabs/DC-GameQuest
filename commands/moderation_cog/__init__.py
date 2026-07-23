94b10eb7bf67097a938094a25fa958b51afe97eb
muhamedlabs <kg7221996@gmail.com>
2026-07-23 21:32:04 +0300

    Add verification

--- commands/moderation_cog/__init__.py
+++ commands/moderation_cog/__init__.py
@@ -1,9 +1,11 @@
 from disnake.ext import commands
 from .bot import BotBan
 from .automod import AutoMod
+from .scam import ScamVerification
 
 
 
 def setup(bot: commands.Bot):
     bot.add_cog(BotBan(bot))
     bot.add_cog(AutoMod(bot))
+    bot.add_cog(ScamVerification(bot))
