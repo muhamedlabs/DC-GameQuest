e12c24598e3897210d6b5a881346fa88785fb7d3
muhamedlabs <kg7221996@gmail.com>
2026-08-04 19:56:47 +0300

    Adding a new button

--- commands/button_cog/__init__.py
+++ commands/button_cog/__init__.py
@@ -1,7 +1,9 @@
 from disnake.ext import commands
 from .button_click_1 import CustomOnButtonClick
+from .greeting_button import GreetingButton
 
 
 
 def setup(bot: commands.Bot):
     bot.add_cog(CustomOnButtonClick(bot))
+    bot.add_cog(GreetingButton(bot))
