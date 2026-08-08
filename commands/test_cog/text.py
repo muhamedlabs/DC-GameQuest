5c261a77ac03057c9a5d1bd7cd18a3d81d5bf1bb
muhamedlabs <kg7221996@gmail.com>
2026-08-08 11:41:28 +0300

    some test text

--- commands/test_cog/text.py
+++ commands/test_cog/text.py
@@ -2,7 +2,8 @@ import disnake
 from disnake.ext import commands
 from disnake import Permissions
 from disnake import InteractionContextTypes
-from BANNED_FILES.config import Embed_Color, GROUP_ADMIN_ID
+from commands.information_cog.warnings import no_access_embed, system_error_embed, critical_error_embed, security_block_embed, invalid_input_embed
+from BANNED_FILES.config import Embed_Color, GROUP_ADMIN_ID, ALLOWED_USER_IDS
 
 class TextCommands(commands.Cog):
     def __init__(self, bot: commands.Bot):
@@ -14,10 +15,13 @@ class TextCommands(commands.Cog):
         description="Отправляет заранее заданный текст"
     )
 
-    @commands.contexts(bot_dm=False,  guild=True)
-    @commands.default_member_permissions(moderate_members=True, administrator=True)
+    @commands.contexts(bot_dm=False,  guild=True) # приватка и гільдія дозволи
+    #@commands.default_member_permissions(moderate_members=True, administrator=True) # роли дозволи
 
     async def send_text(self, inter: disnake.AppCmdInter):
+
+        owner = inter.guild.owner.mention if inter.guild and inter.guild.owner else "Нет владельца"
+        admins_mentions = " ".join(f"<@{uid}>" for uid in ALLOWED_USER_IDS)
 
         has_access = (
             any(role.id in GROUP_ADMIN_ID for role in inter.author.roles)
@@ -26,19 +30,13 @@ class TextCommands(commands.Cog):
         )
 
         if not has_access:
-            embed = disnake.Embed(
-                title="<:forbidden:1390972224436965386> Доступ к команде заблокирован",
-                description=(
-                    "У вас **отсутствуют полномочия** для выполнения данного приказа.\n\n"
-                    f">>> Если это ошибка — свяжитесь с администратором: {inter.guild.owner.mention}"
-                ),
-                color=self.embed_color
-            )
+            
+            embed = no_access_embed(self.embed_color, owner, admins_mentions)
 
             await inter.response.send_message(embed=embed, ephemeral=True)
             return
 
         await inter.response.send_message(
             "👋 Добро пожаловать на сервер Game Quest!\nСледите за новостями и оставайтесь с нами!",
-            ephemeral=True
+            ephemeral=False
         )
