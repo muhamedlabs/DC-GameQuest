1b5449728c8ae5c6e616d2923df103b58bb55930
muhamedlabs <kg7221996@gmail.com>
2026-08-08 11:41:52 +0300

    Hmm, there's something wrong with the import

--- commands/dispatcher_cog/__init__.py
+++ commands/dispatcher_cog/__init__.py
@@ -1,9 +1,11 @@
 from disnake.ext import commands
 from .video import VideoIntegration
 from .webhook import WebhookFromDiscord
+from .message import MessagePerson
 
 
 
 def setup(bot: commands.Bot):
     bot.add_cog(VideoIntegration(bot))
     bot.add_cog(WebhookFromDiscord(bot))
+    bot.add_cog(MessagePerson(bot))
